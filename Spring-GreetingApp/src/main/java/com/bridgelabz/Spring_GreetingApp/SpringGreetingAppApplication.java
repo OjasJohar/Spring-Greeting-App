c59e05f59cdd90bba7ad71da15716c0eecb4d25c
OjasJohar <ojasjohar@gmail.com>
2025-02-22 13:36:50 +0530

    [Ojas] Add. Initalized Greeting App Repository

---
+++ Spring-GreetingApp/src/main/java/com/bridgelabz/Spring_GreetingApp/SpringGreetingAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Spring_GreetingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGreetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGreetingAppApplication.class, args);
+	}
+
+}
