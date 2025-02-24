e40d6142febe06a8391a4bdb80bea1dd88d570cd
OjasJohar <ojasjohar@gmail.com>
2025-02-24 15:39:46 +0530

    [Ojas] Add. Ability for the Greeting App to find a Greeting Message by Id in the Repository

--- Spring-GreetingApp/src/main/java/com/bridgelabz/Spring_GreetingApp/controller/GreetingController.java
+++ Spring-GreetingApp/src/main/java/com/bridgelabz/Spring_GreetingApp/controller/GreetingController.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.Spring_GreetingApp.controller;
 
+import com.bridgelabz.Spring_GreetingApp.model.Greeting;
 import com.bridgelabz.Spring_GreetingApp.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class GreetingController {
@@ -35,5 +38,16 @@ public class GreetingController {
     public String getGreetingParams(@RequestParam(value = "firstName",required = false) String firstName,
                                     @RequestParam(value = "lastName",required = false) String lastName){
         return greetingService.getGreeting(firstName,lastName);
+    }
+
+    //UseCase: 5
+    @GetMapping("/greeting/{id}")
+    public String getGreetingById(@PathVariable long id) {
+        Optional<Greeting> greeting = greetingService.findGreetingById(id);
+        if (greeting.isPresent()) {
+            return greeting.get().getMessage();
+        } else {
+            return "Greeting not found";
+        }
     }
 }
