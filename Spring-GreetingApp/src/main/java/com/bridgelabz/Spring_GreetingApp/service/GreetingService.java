fec8220276c4d49f33e1ca339d7da19563749fad
OjasJohar <ojasjohar@gmail.com>
2025-02-24 16:14:03 +0530

    [Ojas] Add. Ability for the Greeting App to Edit a Greeting Messages in the Repository

--- Spring-GreetingApp/src/main/java/com/bridgelabz/Spring_GreetingApp/service/GreetingService.java
+++ Spring-GreetingApp/src/main/java/com/bridgelabz/Spring_GreetingApp/service/GreetingService.java
@@ -32,16 +32,32 @@ public class GreetingService {
         return message;
     }
 
+    //UseCase: 4
     public void saveGreeting(String message) {
         Greeting greeting = new Greeting(message);
         greetingRepository.save(greeting);
     }
 
+    //UseCase: 5
     public Optional<Greeting> findGreetingById(long id){
         return greetingRepository.findById(id);
     }
 
+    //UseCase:6
     public List<Greeting> getAllGreeting(){
         return greetingRepository.findAll();
+    }
+
+    //UseCase: 7
+    public Optional<Greeting> updatingGreeting(long id, String newMessage){
+        Optional<Greeting> existingGreeting=greetingRepository.findById(id);
+        if(existingGreeting.isPresent()){
+            Greeting greeting=existingGreeting.get();
+            greeting.setMessage(newMessage);
+            greetingRepository.save(greeting);
+            return Optional.of(greeting);
+        }else{
+            return Optional.empty();
+        }
     }
 }
